46bca2e69eef8cd66fb5ef5acb9eb591cba76943
thpr282837seyUTI <masimaroseizin@yahoo.co.jp>
2025-05-26 00:33:45 +0900

    Delete kadai_020 directory

--- kadai_020/event.js
+++
@@ -1,9 +0,0 @@
-const btn = document.getElementById('btn');
-
-btn.addEventListener('click', () => {
-  console.log('クリックされました！');
-});
-
-HTML要素.addEventListener('click', () => {
-  'ボタンをクリックしました'
-});

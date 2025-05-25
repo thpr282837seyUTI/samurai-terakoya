0d640ca560d08fccb5feb604a5ff38ef6ec56edf
thpr282837seyUTI <masimaroseizin@yahoo.co.jp>
2025-05-26 00:25:28 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+  console.log('クリックされました！');
+});
+
+HTML要素.addEventListener('click', () => {
+  'ボタンをクリックしました'
+});
